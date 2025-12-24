f5b11c27026f1c7ffa1bdb78fad5f97d3d99cf72
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-24 15:20:01 +0100

    Add listener to get user id for fetch request

--- src/background/background.js
+++ src/background/background.js
@@ -4,3 +4,13 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.action === "get_ds_user_id") {
+    chrome.cookies.getAll({ domain: ".instagram.com" }, (cookies) => {
+      const dsUserCookie = cookies.find(c => c.name === "ds_user_id");
+      sendResponse({ ds_user_id: dsUserCookie ? dsUserCookie.value : null });
+    });
+    return true;
+  }
+});
